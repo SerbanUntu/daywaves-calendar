7ffcd148602ed39654bd97fba09571ecb03c1c33
Șerban Untu <133024956+SerbanUntu@users.noreply.github.com>
2023-12-17 14:38:49 +0200

    fix navbar events
    Closes #3

--- src/components/icons/IconTemplatesLarge.vue
+++ src/components/icons/IconTemplatesLarge.vue
@@ -1,10 +1,10 @@
 <template>
-  <svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70" fill="none">
     <path
       d="M15 53.6667V16.3333C15 15.597 15.5373 15 16.2 15H53.8C54.4628 15 55 15.597 55 16.3333V53.6667C55 54.4031 54.4628 55 53.8 55H16.2C15.5373 55 15 54.4031 15 53.6667Z"
-      stroke="#585858" stroke-width="3" stroke-linejoin="round" />
-    <rect opacity="0.8" x="24.5" y="22" width="21" height="3" rx="1.5" fill="#585858" />
-    <rect x="22.5" y="30" width="25" height="3" rx="1.5" fill="#585858" />
-    <rect opacity="0.8" x="24.5" y="38" width="21" height="3" rx="1.5" fill="#585858" />
+      stroke-width="3" stroke-linejoin="round" />
+    <rect x="22.75" y="30.75" width="24.5" height="1.5" rx="0.75" stroke-width="1.5" />
+    <rect opacity="0.8" x="24.75" y="38.75" width="20.5" height="1.5" rx="0.75" stroke-width="1.5" />
+    <rect opacity="0.8" x="24.75" y="22.75" width="20.5" height="1.5" rx="0.75" stroke-width="1.5" />
   </svg>
 </template>
